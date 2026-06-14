f30c01dab89e67856226aa0abb3158c32b5c1afc
AnaCataVC <anacatalina@outlook.cl>
2026-06-14 19:50:21 -0400

    fix: proxy identity-map assets to fix 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    server: {
+      proxy: {
+        '/identity-map': {
+          target: 'https://identity-map-xi.vercel.app',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/identity-map/, '')
+        },
+        '/assets': {
+          target: 'https://identity-map-xi.vercel.app',
+          changeOrigin: true
+        }
+      }
+    }
+  }
 });
